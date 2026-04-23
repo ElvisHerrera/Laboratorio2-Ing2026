29a0e0ffade538342fc6df8759263b0c422e6793
Elvis Herrera <elvisherreragh@gmail.com>
2026-04-22 22:28:13 -0500

    fix 2 pipeline

--- conftest.py
+++ conftest.py
@@ -25,6 +25,8 @@ def driver():
 	options.add_argument("--headless")
 	options.add_argument("--no-sandbox")
 	options.add_argument("--disable-dev-shm-usage")
+	options.add_argument("--disable-gpu")
+	options.binary_location = "/usr/bin/chromium-browser"
 	driver = webdriver.Chrome(options=options)
 	driver.maximize_window()
 	yield driver
